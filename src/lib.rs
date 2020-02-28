cc931856a7a80c39e22e8b525dfc658f21d8f94f
Ionman64 <PeterPickerill2014@gmail.com>
2020-02-28 23:52:41 +0100

    Started reading data

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,22 @@
-pub mod product;
+#[macro_use]
+extern crate serde;
+
+pub mod main;
 
 #[cfg(test)]
 pub mod tests {
     use super::*;
-    use product::Product;
+    use main::Product;
 
     #[test]
     pub fn product_stock_count() {
-        let product: Product = Product::new(String::from("Cheese"), 10);
+        let product: Product = Product::new(String::from("Cheese"), 10, 20);
         assert_eq!(product.stock_level, 10);
     }
 
     #[test]
     pub fn product_set_name() {
-        let product: Product = Product::new(String::from("Cheese"), 10);
+        let product: Product = Product::new(String::from("Cheese"), 10, 20);
         assert_eq!(product.name, String::from("Cheese"));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
-extern crate squeeky_trolley;
+extern crate csv;
+
+use csv::Reader;
+use serde::{Deserialize};
 
 fn main() {
-    println!("Hello, world!");
+    read_file();
+}
+
+#[derive(Debug, Deserialize, Eq, PartialEq)]
+pub struct Product {
+    pub name: String,
+    #[serde(rename = "current_stock")]
+    pub stock_level: i64,
+    pub max_stock: i64
+}
+
+impl Product {
+    pub fn new(name: String, stock_level: i64, max_stock: i64) -> Product {
+        Product {
+            name: name,
+            stock_level: stock_level,
+            max_stock: max_stock
+        }
+    }
+}
+
+fn read_file() {
+    let mut reader = match Reader::from_path("data/products.csv") {
+        Ok(x) => x,
+        Err(x) => panic!("Cannot read the input file")
+    };
+    for result in reader.deserialize::<Product>() {
+        let record = match result {
+            Ok(x) => println!("{} {}/{}", x.name, x.stock_level, x.max_stock),
+            Err(x) => panic!("{:?}", x)
+        };
+    }
+    println!("Finished reading the file");
 }
